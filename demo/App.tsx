63ae4216ccd2317bec46de5031c7a8c292a3180a
sehilyi <sehilyi@gmail.com>
2025-02-04 13:29:16 -0500

    basic example

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,7 +1,24 @@
+import { GoslingComponent, Theme } from 'gosling.js';
+import patient1 from './data/patient1.json';
 import { fn } from '../src/index';
 
+const F = 15;
+const t: Theme = {
+    base: 'light',
+    axis: { labelFontSize: F },
+    track: { titleFontSize: F },
+    legend: { labelFontSize: F }
+};
+
 function App() {
-    return <div>{fn()}</div>;
+    const edit = (spec: GoslingSpec) => { };
+
+    return (
+        <div>
+            // {fn()}
+            <GoslingComponent spec={patient1} theme={t} />
+        </div>
+    );
 }
 
 export default App;
